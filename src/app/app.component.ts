f38599ff8e5538eca7a0710f104c6f0780c30455
black script <kaantekk36+github2@gmail.com>
2023-06-07 21:07:01 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Products } from './module/products';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  products = Products;
+  basket:Products[] = [];
+
+  setDecrease(product:Products) : void {
+    if(product.quantitiy > 0 ) {
+      product.quantitiy--;
+    }
+    if(product.quantitiy == 0) {
+      let index = this.basket.indexOf(product);
+      this.basket.splice(index, 1);
+    }
+  }
+
+  setIncrease(product:Products) : void {
+    if(!this.basket.includes(product)) {
+      this.basket.push(product);
+    }
+    product.quantitiy++;
+  }
+
+  getTotalPrice(fixed:number) : string {
+    let total = 0;
+    this.basket.forEach(item => {
+      total += item.quantitiy * item.price;
+    });
+
+    return total.toFixed(fixed);
+  }
+
+
+}
+
+type Products = {
+  id: number,
+  name: string,
+  photo: string,
+  price: number,
+  unit: string,
+  quantitiy: number
 }
---
+++ src/app/module/products.ts
@@ -0,0 +1,34 @@
+export const Products = [
+  {
+    id: 1,
+    name: "Karpuz",
+    photo: "karpuz.png",
+    price: 19.99,
+    unit: "KG",
+    quantitiy: 0
+  },
+  {
+    id: 2,
+    name: "Domates",
+    photo: "domates.png",
+    price: 11.25,
+    unit: "KG",
+    quantitiy: 0
+  },
+  {
+    id: 3,
+    name: "Makarna",
+    photo: "makarna.png",
+    price: 5.55,
+    unit: "ADET",
+    quantitiy: 0
+  },
+  {
+    id: 4,
+    name: "Ayran",
+    photo: "ayran.png",
+    price: 3.50,
+    unit: "ADET",
+    quantitiy: 0
+  }
+]
